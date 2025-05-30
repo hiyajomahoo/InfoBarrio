3500d4570a56622b80d366a284971419a26caabf
patow <muras.patricio@tecnica7.edu.ar>
2025-05-30 20:09:39 -0300

    agregado de componente

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import BotonNav from './btnNav'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BotonNav texto="Hola"/>
+      <BotonNav texto="Chau"/>
+      <BotonNav texto="Qué"/>
     </View>
   );
 }
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: "row",
   },
 });
